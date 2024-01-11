19208f77a87cf2dd0c44d281f582ff5b5543dbd0
Luis Romero <lueduar15@gmail.com>
2024-01-11 00:52:51 -0400

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Genre = require('./Genre')
+const Actor = require('./Actor')
+const Director = require('./Director')
+const Movie = require('./Movie')
+
+Actor.belongsToMany(Movie, {through: 'ActorsMovies'})
+Movie.belongsToMany(Actor, {through: 'ActorsMovies'})
+
+Genre.belongsToMany(Movie, {through: 'GenresMovies'})
+Movie.belongsToMany(Genre, {through: 'GenresMovies'})
+
+Director.belongsToMany(Movie, {through: 'DirectosMovies'})
+Movie.belongsToMany(Director, {through: 'DirectosMovies'})
